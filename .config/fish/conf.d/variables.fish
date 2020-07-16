f62d3ab94dec126b21af1c66d3bad6a6f1820368
Tuan Ha <tuanht@users.noreply.github.com>
2020-07-16 13:24:42 +0700

    set GPG pinentry for console mode

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,3 +1,6 @@
 set --export EDITOR nvim
-set --export GPG_TTY (tty)
-set --export PINENTRY_USER_DATA USE_TTY=1
+
+if status --is-login
+    set --export GPG_TTY (tty)
+    set --export PINENTRY_USER_DATA USE_TTY=1
+end
