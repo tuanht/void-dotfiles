f4edb653572804a2341ba6f3537758b72baa2deb
Tuan Ha <tuanht@users.noreply.github.com>
2020-08-04 19:11:11 +0700

    cleanup pull todo & pass

--- scripts/pull-bare-data.sh
+++ scripts/pull-bare-data.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 OS_TYPE="$(uname)"
-PASS_PREFIX="$HOME/.password-store"
 YADM_PREFIX="$HOME/.config/yadm/repo.git"
 NOTE_PREFIX="$HOME/notes"
-TODO_PREFIX="$HOME/todo"
 NOW=$(date +%s)
 RANGE=$(expr 60 \* 60 \* 24)
 
@@ -18,17 +16,6 @@ function date_modify() {
             ;;
     esac
 }
-
-# Pass data
-echo 'Checking pass status...'
-
-last_pulled=$(date_modify $PASS_PREFIX/.git/FETCH_HEAD)
-diff=$(expr $NOW - $last_pulled)
-
-if [ "$diff" -gt "$RANGE" ]; then
-    echo 'Updating pass...'
-    pass git pull --ff-only
-fi
 
 # dotfiles
 echo 'Checking dotfiles status...'
@@ -52,13 +39,3 @@ if [ "$diff" -gt "$RANGE" ]; then
     git -C $NOTE_PREFIX pull --ff-only
 fi
 
-# todo
-# echo 'Checking todo status...'
-
-# last_pulled=$(date_modify $TODO_PREFIX/.git/FETCH_HEAD)
-# diff=$(expr $NOW - $last_pulled)
-
-# if [ "$diff" -gt "$RANGE" ]; then
-#     echo 'Updating todo...'
-#     git -C $TODO_PREFIX pull --ff-only
-# fi
