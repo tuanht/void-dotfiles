84ee91459c25ba29f2a7d78debf1b7cd3e0f25d9
Tuan Ha <tuanht@users.noreply.github.com>
2020-08-09 11:09:23 +0700

    remove pull pass data from fish

--- .config/fish/conf.d/pull-bare-data.fish
+++ .config/fish/conf.d/pull-bare-data.fish
@@ -1,9 +1,7 @@
 #!/usr/bin/env fish
 
-set PASS_PREFIX "$HOME/.password-store"
 set YADM_PREFIX "$HOME/.config/yadm/repo.git"
 set NOTE_PREFIX "$HOME/notes"
-set TODO_PREFIX "$HOME/todo"
 set NOW (date +%s)
 set RANGE (expr 60 \* 60 \* 24)
 
@@ -14,17 +12,6 @@ function date_modify
         case '*'
             echo (stat -f '%m' $argv)
     end
-end
-
-# Pass data
-echo 'Checking pass status...'
-
-set last_pulled (date_modify $PASS_PREFIX/.git/FETCH_HEAD)
-set diff (expr $NOW - $last_pulled)
-
-if test "$diff" -gt "$RANGE"
-    echo 'Updating pass...'
-    pass git pull --ff-only
 end
 
 # dotfiles
