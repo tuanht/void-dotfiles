1a4135b815ea70c97c77a5ea8c0b50a211b3f0b6
Tuan Ha <tuanht@users.noreply.github.com>
2021-10-21 11:17:22 +0700

    update docker scripts

--- scripts/docker-data-init.sh
+++ scripts/docker-data-init.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+git_current_branch=$(git branch --show-current)
+export DATABASE_NAME="${DATABASE_BASE_NAME}_$git_current_branch"
+export DATABASE_ROOT_HOST=%
+
 docker-compose exec php-fpm bin/console doctrine:database:drop --force --if-exists
 docker-compose exec php-fpm bin/console doctrine:database:create --if-not-exists
 docker-compose exec php-fpm bin/console doctrine:migration:migrate --no-interaction
--- scripts/docker-start-server.sh
+++ scripts/docker-start-server.sh
@@ -3,6 +3,10 @@
 git_current_branch=$(git branch --show-current)
 export DATABASE_NAME="${DATABASE_BASE_NAME}_$git_current_branch"
 export DATABASE_ROOT_HOST=%
-docker-compose stop
+
+# Use for local env without docker
+export DATABASE_URL="$DATABASE_BASE_URL/${DATABASE_BASE_NAME}_$git_current_branch"
+
+docker-compose down
 docker-compose up -d
 
