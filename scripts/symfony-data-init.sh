767ec791f205fc0d9eb7394df1f8a5523c8cbffb
Tuan Ha <tuanht@users.noreply.github.com>
2021-10-22 18:43:24 +0700

    update symfony script

--- scripts/symfony-data-init.sh
+++ scripts/symfony-data-init.sh
@@ -2,7 +2,7 @@
 
 git_current_branch=$(git branch --show-current)
 export DATABASE_URL="$DATABASE_BASE_URL/${DATABASE_BASE_NAME}_$git_current_branch"
-symfony console doctrine:database:drop --force
+symfony console doctrine:database:drop --force --if-exists
 symfony console doctrine:database:create --if-not-exists
 symfony console doctrine:migration:migrate --no-interaction
 symfony console doctrine:fixtures:load --no-interaction --append
