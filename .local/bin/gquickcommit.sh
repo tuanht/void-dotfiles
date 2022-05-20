fe6d84872bbac42234d913f9f58e3b1eacf4e47a
Tuan Ha <tuanht@users.noreply.github.com>
2022-05-20 15:52:07 +0700

    Configs git flow for private repos

---
+++ .local/bin/gquickcommit.sh
@@ -0,0 +1,16 @@
+# vim: set ft=bash:
+#!/bin/bash
+
+gstatus=`git status --porcelain`
+
+if [ ${#gstatus} -ne 0 ]
+then
+
+    git add --all
+    git commit -m "$gstatus"
+
+	git pull
+    git push
+
+fi
+
