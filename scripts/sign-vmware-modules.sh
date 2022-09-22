d504d53fa8f5231b8cf4550c840adde7fb7d582b
Tuan Ha <tuanht@users.noreply.github.com>
2022-09-22 15:37:05 +0700

    check key files exists

--- scripts/sign-vmware-modules.sh
+++ scripts/sign-vmware-modules.sh
@@ -5,6 +5,21 @@ MODULE_DIR=/lib/modules/$(uname -r)/misc/
 PRIVATE_KEY_FILE=${1}
 PUBLIC_KEY_FILE=${2}
 
+if [ ! -d $MODULE_DIR ]; then
+    echo "$MODULE_DIR does not exists."
+    exit 1
+fi
+
+if [ ! -s "$PRIVATE_KEY_FILE" ]; then
+    echo "PRIVATE_KEY_FILE does not exists."
+    exit 2
+fi
+
+if [ ! -s "$PUBLIC_KEY_FILE" ]; then
+    echo "PUBLIC_KEY_FILE does not exists."
+    exit 3
+fi
+
 /usr/src/kernels/$(uname -r)/scripts/sign-file sha256 \
     $PRIVATE_KEY_FILE \
     $PUBLIC_KEY_FILE \
